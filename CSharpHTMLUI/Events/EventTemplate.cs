911d1c1b7dfeb454305d44cbd2482a16bc729150
Damian Guth <guth-d@web.de>
2018-06-09 13:56:07 +0200

    Add event interface

---
+++ CSharpHTMLUI/Events/EventTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHTMLUI.Events
+{
+    public interface EventTemplate
+    {
+        string GetId();
+        void OnClick();
+    }
+}
