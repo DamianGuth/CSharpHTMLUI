a2adfab5c5fde9037f7da94480066cb4eb910bb9
Damian Guth <guth-d@web.de>
2018-06-09 18:56:16 +0200

    Add generic class

---
+++ CSharpHTMLUI/Generic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHTMLUI
+{
+    /// <summary>
+    /// This class contains helper methods
+    /// </summary>
+    public class Generic
+    {
+        private static Random Random = new Random();
+
+        /// <summary>
+        /// The length to use for generated IDs
+        /// </summary>
+        public static int IDLength = 32;
+
+        /// <summary>
+        /// Returns a random string
+        /// </summary>
+        /// <param name="length">Defines the length of the random string</param>
+        public static string GetRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
