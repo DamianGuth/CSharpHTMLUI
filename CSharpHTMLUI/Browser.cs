08a84f4f1b20ab885b391076c44675e944469e6b
Damian Guth <guth-d@web.de>
2018-06-09 15:29:50 +0200

    Add browser class to handle the config

---
+++ CSharpHTMLUI/Browser.cs
@@ -0,0 +1,27 @@
+﻿using IEFixLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHTMLUI
+{
+    public class Browser
+    {
+        public static void Initialize()
+        {
+            string processName = Process.GetCurrentProcess().ProcessName + ".exe";
+
+            BrowserSetup.SetupBrowser();
+            BrowserSetup.SetupBrowser(processName);
+            BrowserSetup.SetupBrowser(processName, 11);
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            Form1.webBrowser.Navigate(baseDirectory + "test.html");
+            Form1.webBrowser.ObjectForScripting = new Logger();
+        }
+    }
+}
