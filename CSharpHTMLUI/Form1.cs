893b47858262d2a0bee1528c770b18ad399961fc
Damian Guth <guth-d@web.de>
2018-06-09 11:46:24 +0200

    Add basic concept

---
+++ CSharpHTMLUI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IEFixLib;
+
+namespace CSharpHTMLUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string processName = Process.GetCurrentProcess().ProcessName + ".exe";
+
+            BrowserSetup.SetupBrowser();
+            BrowserSetup.SetupBrowser(processName);
+            BrowserSetup.SetupBrowser(processName, 11);
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            webBrowser1.Navigate(baseDirectory + "test.html");
+            webBrowser1.ObjectForScripting = new Logger();
+            //webBrowser1.DocumentText = "<script>external.log('TEST');</script>";
+        }
+
+    }
+}
